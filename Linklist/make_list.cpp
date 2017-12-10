dc68cf8bdbeef55c83ce8afaac4dd4b7980911be
Ruhul amin <ruhul.cse7862@gmail.com>
2017-12-11 01:28:06 +0600

    Create make_list.cpp

---
+++ Linklist/make_list.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct node{
+int data;
+struct node*link;
+};
+struct node*head;
+
+void print()
+{
+struct node*temp;
+temp=head;
+while()
+    {
+    printf("%d",temp->data);
+    temp=temp->link;
+}
+
+}
+
+int main()
+{
+    head=NULL;
+    head=(struct node*)malloc(sizeof(struct node ));
+    head->data=2;
+    head->link=(struct node*)malloc(sizeof(struct node ));
+    head->link->data=5;
+    head->link->link=NULL;
+    return 0;
+}
